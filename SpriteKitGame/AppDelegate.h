9bec7f0463d3257c1cf22c06c85632ed7e5c4a45
Skyler Lauren <swiftmist29@gmail.com>
2014-05-02 20:04:33 -0500

    Initial Commit

---
+++ SpriteKitGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitGame
+//
+//  Created by Skyler Lauren on 5/2/14.
+//  Copyright (c) 2014 Sky Mist Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpriteKitGame
+//
+
+//  Copyright (c) 2014 Sky Mist Development. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
